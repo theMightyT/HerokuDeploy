c3e8d5f5cf460afad6a67aa313ec722340d4a1c8
halvas_trevor@halvas.ca <trevorvanrys@Trevors-MacBook-Pro-2.local>
2018-09-04 10:06:48 -0400

    adding environment variable

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const port=process.env.PORT || 3000
 
+let msg = process.env.MESSAGE || "Hello World!"
+
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
-	res.end('<h1>Hello World! This is my Heroku app! I like Heroku!</h1>');
+	res.end(`<h1>${msg} This is my Heroku app! I like Heroku!</h1>`);
 });
 
 server.listen(port,() => {
